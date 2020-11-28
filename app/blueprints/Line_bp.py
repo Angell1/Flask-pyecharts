21cf4551b5f21e6d50c0cd2737497e75ef61af96
ling <943955239@qq.com>
2020-11-28 23:37:09 +0800

    编写多折线接口

--- app/blueprints/Line_bp.py
+++ app/blueprints/Line_bp.py
@@ -8,7 +8,7 @@ from pyecharts import options as opts
 from pyecharts.render import make_snapshot
 from pyecharts.globals import ThemeType
 from snapshot_selenium import snapshot
-from app.validates.validate import norlineSchema
+from app.validates.validate import norlineSchema,mullineSchema
 
 Linebp = Blueprint("Line", __name__, url_prefix="/Line")
 
@@ -55,7 +55,7 @@ def create_mulline():
     returnres = {"state": 200, "msg": "succsuful", "result": ''}
     print(data)
     try:
-        schema = norlineSchema()
+        schema = mullineSchema()
         data = schema.load(data)
         data = schema.dump(data)
         print(data)
@@ -66,13 +66,14 @@ def create_mulline():
         return jsonify(returnres)
     line = (
         Line(init_opts=opts.InitOpts(theme=ThemeType.DARK))
-            .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
-            .add_yaxis("商家A", [5, 20, 36, 10, 10, 100])
-            .add_yaxis("商家B", [55, 60, 16, 20, 15, 80])
+            .add_xaxis(data['xaxis'])
             .set_global_opts(
-                title_opts=opts.TitleOpts(title="mulLine"),
+                title_opts=opts.TitleOpts(title=data['maintitle']),
         )
     )
+    for i in range(len(data['typelist'])):
+        line.add_yaxis(data['typelist'][i],data['yaxis'][i])
+
     # 输出保存为图片
     imagesname = str(uuid.uuid4())
     make_snapshot(engine=snapshot, file_name=line.render(), output_name="../charts/image/%s.png" % imagesname,is_remove_html=True)
--- client/client.py
+++ client/client.py
@@ -1,7 +1,25 @@
-import json
+import json,os,base64
 import requests
 
-url = 'http://127.0.0.1:5000/Line/norline'
-data = {'xaxis': ["6月", "7月", "8月", "9月", "10月", "11月"],'typelist':['商家A'],'yaxis':[268, 278, 369, 310, 504, 1056],'maintitle':'销量图','mintitle':'小标题'}
-r = requests.get(url, data=json.dumps(data))
-print(r.text)
+
+
+def req_norLine(url,data,imagename):
+    url = 'http://127.0.0.1:5000/Line/norline'
+    data = {'xaxis': ["6月", "7月", "8月", "9月", "10月", "11月"],'typelist':['商家A'],'yaxis':[268, 278, 369, 310, 504, 1056],'maintitle':'销量图','mintitle':'小标题'}
+    r = requests.get(url, data=json.dumps(data))
+    res = json.loads(r.text)
+    with open(imagename, 'wb') as f:
+        f.write(base64.b64decode(res['result']))
+
+
+
+def req_mulLine(url,data,imagename):
+    url = 'http://127.0.0.1:5000/Line/mulline'
+    data = {'xaxis': ["6月", "7月", "8月", "9月", "10月", "11月"],'typelist':['商家A','商家B'],'yaxis':[[268, 278, 369, 310, 504, 1056],[55, 60, 16, 20, 15, 80]],'maintitle':'销量图','mintitle':'小标题'}
+    r = requests.get(url, data=json.dumps(data))
+    res = json.loads(r.text)
+    with open(imagename, 'wb') as f:
+        f.write(base64.b64decode(res['result']))
+
+
+req_mulLine(url='',data='',imagename='test1.png')
